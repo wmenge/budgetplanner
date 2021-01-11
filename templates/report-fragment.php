5d5c78588bf4d571281b07d2c4eee24c5650bef6
wilco menge <wilcomenge@gmail.com>
2021-01-11 16:20:30 +0100

    code cleanup

--- templates/report-fragment.php
+++ templates/report-fragment.php
@@ -3,17 +3,6 @@
 <?= $this->fetch("report-tab-navigation-fragment.php", [ 'type' => $type ]); ?>
 
 <div class="row py-3 align-items-center  ">
-  <!--<div class="col-2">
-  	<div class="form-floating">
-	    <select id="year" name="year" class="form-select" onchange="filter(this)">
-	    	<option value="">Choose a filter</option>
-	  		<?php foreach ($years as $year): ?>
-				  <option value="<?= $year->period ?>"><?= $year->period ?></option>
-			  <?php endforeach; ?>
-		  </select>
-		  <label for="floatingSelect">Year</label>
-	</div>
-  </div>-->
   <div class="col-2">
   	<div class="form-floating">
     <select id="month" name="month" class="form-select" onchange="filter(this)">
@@ -28,16 +17,16 @@
   </div>
 </div>
 
-<? switch($type): ?>
-<? case 'expenses': ?>
+<?php switch($type): ?>
+<?php case 'expenses': ?>
   <?= $this->fetch("report-categories-fragment.php", [ 'type' => $type ]); ?>
   <? break; ?>
-<? case 'income': ?>
+<?php case 'income': ?>
   <?= $this->fetch("report-categories-fragment.php", [ 'type' => $type ]); ?>
-  <? break; ?>
-<? case 'periods': ?>
+  <?php break; ?>
+<?php case 'periods': ?>
   <?= $this->fetch("report-periods-fragment.php"); ?>
   <? break; ?>
-<? break; default: ?>
+<?php break; default: ?>
   Should not happen
-<?php endswitch ?>
+<?php break; endswitch ?>
